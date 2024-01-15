e53898a7101a535fb75527a4b828fbb9c3e75600
amandawangg <amanda.epnk@gmail.com>
2024-01-14 19:40:47 -0800

    initial commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
-import { useState } from "react";
 
 export const Banner = () => {
-    const toRotate = [ "Recent Grad from USF", "UX/UI Designer", "Frontend Engineer", "Software Engineer"];
-    const [text, setText] = useState('');
     
     return (
         <section className="banner" id="home">
@@ -13,7 +10,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}> 
                         <spam className="tagline">Welcome to my Portfolio</spam>
-                        <h1>{`Hi I'm Amanda `}<span className="wrap">{text}</span></h1>
+                        <h1>{`Hi I'm Amanda `}</h1>
                         <p>Description goes here</p>
                         <button onClick={(() => console.log('connect'))}>Let's Connect <ArrowRightCircle size={25} /></button>
                     </Col>
