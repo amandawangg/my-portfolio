1f54b61c4fc50ee75908e0040ba6c6688849a031
amandawangg <amanda.epnk@gmail.com>
2024-01-14 19:37:09 -0800

    initial commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,45 +1,12 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const Banner = () => {
     const toRotate = [ "Recent Grad from USF", "UX/UI Designer", "Frontend Engineer", "Software Engineer"];
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
-    const period = 2000; 
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
     
-    // useEffect(() => {
-    //     let ticker = setInterval(() => {
-    //         tick();
-    //     }, delta)
-
-    //     return () => {clearInterval(ticker)};
-    // }, [text])
-
-    // const tick = () => {
-    //     let i = loopNum %toRotate.length;
-    //     let fullText = toRotate[i];
-    //     let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
-        
-    //     setText(updatedText);
-
-    //     if (isDeleting) {
-    //         setDelta(prevDelta => prevDelta / 2)
-    //     }
-
-    //     if (!isDeleting && updatedText === fullText) {
-    //         setIsDeleting(true);
-    //         setDelta(period);
-    //     } else if(isDeleting && updatedText === '') {
-    //         setIsDeleting(false);
-    //         setLoopNum(loopNum + 1);
-    //         setDelta(500);
-    //     }
-    // }
-
     return (
         <section className="banner" id="home">
             <Container>
