37d2f6d9ee4f441daacfca03396a962939d1d64c
amandawangg <amanda.epnk@gmail.com>
2024-01-15 13:29:10 -0800

    worked on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState, useEffect } from 'react';
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
+import linkedInIcon from '../assets/img/linkedin-icon.png';
+import githubIcon from '../assets/img/github-icon.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -29,7 +30,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" style={{ width: '70px', height: 'auto' }}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" >
             <span className="navbar-toggle-icon"></span>
@@ -38,15 +39,13 @@ export const NavBar = () => {
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('skills')}>Projects</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className='social-icon'>
-                <a href="https://www.linkedin.com/in/amanda-wang04/"><img src={navIcon1} alt="" /> </a>
-                {/* <a href="#"><img src={navIcon2} alt="" /> </a>
-                <a href="#"><img src={navIcon3} alt="" /> </a> */}
+                <a href="https://www.linkedin.com/in/amanda-wang04/"><img src={linkedInIcon} alt="" /></a>
+                <a href="https://github.com/amandawangg"><img src={githubIcon} alt="" /></a>
             </div>
-
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span> </button>
           </span>
         </Navbar.Collapse>
