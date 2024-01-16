cb5cf1917082e1977e1e6ac802d5f5d418664131
amandawangg <amanda.epnk@gmail.com>
2024-01-16 10:31:05 -0800

    added Resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,7 +40,7 @@ export const NavBar = () => {
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('home')}>About</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('projects')}>Projects</Nav.Link>
-            <Nav.Link href="#resume" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('resume')}>Resume</Nav.Link>
+            <Nav.Link href="https://drive.google.com/file/d/1wtzl7K8zomnm0PoU_nnarJUIOFvTfbTx/view?usp=sharing" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivateLink('resume')}target="_blank" >Resume</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className='social-icon'>
