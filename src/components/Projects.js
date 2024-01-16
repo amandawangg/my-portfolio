b4aa8158fda26e61cee5e02be8f01745eb20858d
amandawangg <amanda.epnk@gmail.com>
2024-01-15 17:21:09 -0800

    adding Projects

---
+++ src/components/Projects.js
@@ -0,0 +1,76 @@
+import { Tab, TabContainer, Nav, Container, Row, Col } from "react-bootstrap";
+import projImg1 from "../assets/img/project-img1.png";
+import { ProjectCard } from "./ProjectCard";
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+    const projects = [
+        {
+          title: "Search Engine",
+          description: "Backend Project",
+          imgUrl: projImg1,
+        },
+        {
+          title: "iOS App",
+          description: "Frontend Project",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Project",
+          description: "Backend Project",
+          imgUrl: projImg1,
+        },
+      ];
+    
+      return (
+        <section className="project" id="projects">
+          <Container>
+            <Row>
+              <Col size={12}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                    <h2>Projects</h2>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                      <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                      <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                        <Tab.Pane eventKey="first">
+                          <Row>
+                            {
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                            }
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="section">
+                          <p>Description</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <p>Description</p>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Tab.Container>
+                  </div>}
+                </TrackVisibility>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      )
+}
